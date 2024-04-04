eac88778b6926981f2ca9032e944cb071a4f58a0
rodrigototy <rodrigototy@yahoo.com.br>
2024-04-04 17:57:41 -0300

    Teste JS dynamic header and footer in pages

---
+++ .history/tourism-agency/static/js/submain_20240404174907.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+  const src 
+  // Carrega o header
+  fetch("../static/templates/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      header.innerHTML = data;
+    });
+
+  // Carrega o footer
+  fetch("../static/templates/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      footer.innerHTML = data;
+    });
+});

1acabbdc40ce1aa3d00908ce95a6e49110e8abb2
rodrigototy <rodrigototy@yahoo.com.br>
2024-04-05 12:04:45 -0300

    Implantação headerFooter.js

---
+++ tourism-agency/static/js/headerFooter.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+
+  // Obtém o caminho relativo para os templates e imagens com base na localização da página
+  let templatesPath, imagePath, hrefPath;
+  console.log(window.location.pathname);
+
+  if (
+    window.location.pathname === "/tourism-agency/" ||
+    window.location.pathname === "/tourism-agency/index.html" ||
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/"
+  ) {
+    templatesPath = "static/templates/";
+    imagePath = "static/img/";
+    hrefPath = "index.html";
+  } else {
+    templatesPath = "../static/templates/";
+    imagePath = "../static/img/";
+    hrefPath = "../index.html";
+  }
+
+  // Carrega o header
+  fetch(templatesPath + "header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      // Substitui os caminhos das imagens no conteúdo do header
+      data = data.replace(/src="static\/img\//g, `src="${imagePath}`);
+      data = data.replace(/href="index.html/g, `href="${hrefPath}`);
+      header.innerHTML = data;
+    });
+
+  // Carrega o footer
+  fetch(templatesPath + "footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      // Substitui os caminhos das imagens no conteúdo do footer
+      data = data.replace(/src="static\/img\//g, `src="${imagePath}`);
+      footer.innerHTML = data;
+    });
+});
